5e03c68ca79f03efbe79636c850deba96478295c
bourbonracer <bourbonracer@gmail.com>
2020-10-01 10:03:17 -0700

    Query URL

---
+++ static/js/logic.js
@@ -0,0 +1,7 @@
+// Query URL
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
+
+// Get request to query URL 
+d3.json(queryUrl, function(data) {
+    
+})
